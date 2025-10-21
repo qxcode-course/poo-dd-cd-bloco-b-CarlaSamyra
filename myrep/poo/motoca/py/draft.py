053c87848d8c4657d5f9051b96d9fa45c58e38ab
Samyra Torres <samyratorres476@gmail.com>
2025-10-20 22:07:31 -0300

    Atividade da motoca finalizada

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -5,6 +5,12 @@ class Pessoa:
 
     def __str__ (self):
         return f"{self.__name}:{self.__age}"
+    
+    def get_name (self):
+        return self.__name
+    
+    def get_age (self):
+        return self.__age
 
 class Moto:
     def __init__ (self, person: str, potencia: int, tempo: int):
@@ -41,8 +47,27 @@ class Moto:
     def buy (self, buy: int):
         self.__tempo += buy
         return self.__tempo
-
     
+    def drive (self, amount: int):
+        if self.__tempo == 0:
+            print ("fail: buy time first")
+            return
+        if self.__person == None:
+            print("fail: empty motorcycle")
+            return
+        if self.__person.get_age() > 10:
+            print ("fail: too old to drive")
+            return
+        if amount > self.__tempo:
+            print (f"fail: time finished after {self.__tempo} minutes")
+            self.__tempo = 0
+            return
+        self.__tempo -= amount
+
+    def honk (self):
+        buzina = "P" + ("e" * self.__potencia) + "m"
+        return buzina
+
 
 def main():
     moto = Moto ("", 1, 0)
@@ -56,17 +81,22 @@ def main():
             print(moto)
         elif args[0] == "enter":
             nome = args[1]
-            idade = args[2]
+            idade = int(args[2])
             pessoa = Pessoa (nome, idade)
             moto.enter(pessoa)
         elif args[0] == "init":
-            init = args[1]
+            init = int(args[1])
             moto.init(init)
         elif args[0] == "leave":
             moto.leave()
         elif args[0] == "buy":
             buy = int(args[1])
             moto.buy(buy)
+        elif args[0] == "drive":
+            amount: int = int(args[1])
+            moto.drive(amount)
+        elif args[0] == "honk":
+            print(moto.honk())
         else:
             print("fail: comando inválido")
 
